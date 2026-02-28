6396b5f94cffba34f9691054835542be7192291a
Samad Ajijola <samadora4life@yahoo.com>
2026-02-28 14:05:49 +0100

    Demo card Updated

--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 const userCtrl = require("../controllers/userCtrl");
 const eventCtrl = require("../controllers/eventCtrl");
 const upload = require("../middlewares/upload");
+const projectCtrl = require("../controllers/projectCtrl")
+const statsCtrl = require("../controllers/statsCtrl")
 
 router.post("/register", userCtrl.register);
 router.post("/login", userCtrl.login);
+//Events
 router.get("/event", eventCtrl.getAll);
 router.get("/event/:id", eventCtrl.getById)
 router.put("/event/:id", eventCtrl.update);
@@ -18,6 +21,16 @@ router.post(
   ]),
   eventCtrl.createEvent
 );
+
+//Project
+router.get("/project", projectCtrl.getAll);
+router.post("/project", projectCtrl.create)
+router.put("/project/:id", projectCtrl.update);
+router.delete("/project/:id", projectCtrl.delete);
+
+// Stats
+router.get("/stats", statsCtrl.get);
+router.put("/stats", statsCtrl.update);
 
 module.exports = router;
 
