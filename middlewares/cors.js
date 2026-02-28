48da95a338647a23672c7cf3b823a3c8095d119d
Samad Ajijola <samadora4life@yahoo.com>
2026-02-28 15:27:59 +0100

    Versel link added2

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,21 @@
-const cors = require('cors')
+const cors = require('cors');
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://igala-wikimedia.vercel.app"
+];
 
 const corsOptions = {
-  origin: process.env.CLIENT_ORIGIN || 'http://localhost:5173' || "https://igala-wikimedia.vercel.app/",
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps, Postman)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("CORS not allowed for this origin"));
+    }
+  },
   credentials: true,
-}
+};
 
-module.exports = cors(corsOptions)
+module.exports = cors(corsOptions);
