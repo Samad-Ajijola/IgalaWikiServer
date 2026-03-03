51c116eaf1cf6664c3a61ef9596da73fdefac868
emmanuel sam <fndm4@gmail.com>
2026-03-03 21:33:24 +0100

    optimized image

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,13 +1,27 @@
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("./cloudinary"); // your cloudinary config
+const cloudinary = require("./cloudinary");
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: {
+  params: async (req, file) => ({
     folder: "IgalaEventsFolder",
     allowed_formats: ["jpg", "jpeg", "png", "webp"],
-  },
+
+    // 🚀 Auto optimization
+    transformation: [
+      {
+        width: 1200,       
+        crop: "limit",      
+      },
+      {
+        quality: "auto",    
+      },
+      {
+        fetch_format: "auto", 
+      },
+    ],
+  }),
 });
 
 const upload = multer({ storage });
